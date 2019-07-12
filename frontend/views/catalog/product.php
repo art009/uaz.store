59edc5534f0056ee972caf0c5e42cd5986cdceb8
Mikhail Kuklin <mihan007@ya.ru>
2019-07-12 08:20:10 +0400

    Вывод аналогичных товаров - смена отображения блока аналогичные товары
    re #1

--- frontend/views/catalog/product.php
+++ frontend/views/catalog/product.php
@@ -3,6 +3,7 @@
 use common\models\ManualProduct;
 use yii\bootstrap\Html;
 use yii\widgets\Breadcrumbs;
+use frontend\widgets\ProductItem;
 
 /* @var $this yii\web\View */
 /* @var $category \common\models\CatalogCategory */
@@ -91,59 +92,15 @@ $this->params['breadcrumbs'] = $product->createBreadcrumbs();
     <div>
         <?php if (sizeof($product->similarProducts) > 0): ?>
             <h2 style="margin-top: 0">Аналогичные товары</h2>
-            <div id="similar-products" class="manual-product-view grid-view">
-                <table class="table table-striped table-bordered">
-                    <?php
-                    /**
-                     * @var $similarProduct \common\models\CatalogProduct
-                     */
-                    ?>
-                    <?php $counterSimilar = 0; ?>
-                    <?php foreach ($product->similarProducts as $similarProduct): ?>
-                        <?php if (sizeof($similarProduct->categories) == 0) {
-                            continue;
-                        } ?>
-                        <?php $counterSimilar++;
-                        if ($counterSimilar > 5) {
-                            continue;
-                        } ?>
-                        <tr class="manual-product-row">
-                            <td style="vertical-align: middle; text-align: center">
-                                <?php if ($similarProduct->images): ?>
-                                    <?php $productImage = $similarProduct->images[0] ?>
-                                    <div class="product-image">
-                                        <a href="<?php echo $similarProduct->getImagePath(false); ?>">
-                                            <img src="<?php echo $similarProduct->getImagePath(); ?>" style="width: 88px;" alt="<?php echo $similarProduct->title; ?>"/>
-                                        </a>
-                                    </div>
-                                <?php else: ?>
-                                    <img src="/img/empty-s.png" alt="<?php echo $similarProduct->title; ?>">
-                                <?php endif; ?>
-                            </td>
-                            <td style="vertical-align: middle; width: 40%">
-                                <?php echo \yii\helpers\Html::a($similarProduct->title, [
-                                    'catalog/product',
-                                    'id' => $similarProduct->id,
-                                    'categoryId' => $similarProduct->categories[0]->id
-                                ], ['class' => 'open-catalog', 'target' => '_blank']); ?>
-                            </td>
-                            <td>
-                                <?php echo $similarProduct->shop_code; ?>
-                            </td>
-                            <td>
-                                <span><?php echo $similarProduct->price; ?></span> руб
-                            </td>
-                            <td style="width: 15%; text-align: center">
-                                <div class="site-btn add-cart-product" data-id="<?php echo $product->id; ?>">
-                                    Добавить в корзину
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-                <?php if ($counterSimilar > 5): ?>
-                    <?php echo Html::a('Все аналогичные товары', ['/catalog/similar', 'id' => $product->id],
-                        ['class' => 'site-btn']) ?>
+            <div id="similar-products" class="similar-product-view grid-view">
+                <?php foreach ($product->similarProducts as $i => $similarProduct): ?>
+                    <?php if ($i >= 5) {
+                        continue;
+                    } ?>
+                    <?php echo ProductItem::widget(['product' => $similarProduct]); ?>
+                <?php endforeach; ?>
+                <?php if (sizeof($product->similarProducts) > 5): ?>
+                    <?php echo Html::a('Все аналогичные товары', ['/catalog/similar', 'id' => $product->id], ['class' => 'similar-product-btn site-btn']) ?>
                 <?php endif; ?>
             </div>
         <?php endif ?>
