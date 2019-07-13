b4bd7ea61b82fe0ce0af819881e62c43ab8ace47
Mikhail Kuklin <mihan007@ya.ru>
2019-07-13 10:31:43 +0400

    Изменить UI отображения строк в чертежах на странице товары close #25

--- frontend/views/catalog/product.php
+++ frontend/views/catalog/product.php
@@ -15,79 +15,81 @@ $this->params['breadcrumbs'] = $product->createBreadcrumbs();
 ?>
 <div class="category-product" style="margin-left: 10px; margin-right: 10px">
     <h1><?php echo $this->title; ?></h1>
-    <div style="display: flex">
-        <div style="width: 60%">
-            <div class="product-content">
-                <div class="product-images" style="vertical-align: center; text-align: center">
-                    <?php if ($product->images): ?>
-                        <?php foreach ($product->images as $productImage): ?>
-                            <div class="product-image">
-                                <a href="<?php echo $productImage->getImagePath(false); ?>" data-fancybox="1"
-                                   rel="group">
-                                    <img src="<?php echo $productImage->getImagePath(); ?>"
-                                         alt="<?php echo $product->title; ?>"/>
-                                </a>
-                            </div>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <img src="/img/empty-s.png" alt="<?php echo $product->title; ?>">
-                    <?php endif; ?>
-                </div>
-                <div class="product-info">
-                    Код на сайте: <b><?php echo $product->getCode(); ?></b>
-                    <br/>
-                    <?php if ($product->shop_code): ?>
-                        Артикул: <b><?php echo $product->shop_code; ?></b>
-                        <br/>
-                    <?php endif; ?>
-                    Цена: <b><span><?php echo $product->price; ?></span> руб</b>
-                    <br/>
-                    <div class="site-btn add-cart-product" data-id="<?php echo $product->id; ?>">
-                        Добавить в корзину
-                    </div>
-                </div>
-                <?php if ($product->oversize): ?>
-                    <?php echo Html::icon('scale', [
-                        'class' => 'bulky-product',
-                        'data-tooltip' => 'tooltip',
-                        'data-trigger' => 'hover',
-                        'data-placement' => 'right',
-                        'title' => 'Крупногабаритный товар',
-                    ]); ?>
-                <?php endif; ?>
-            </div>
-            <?php if (sizeof($product->manuals) > 0): ?>
-                <div class="manual-product-view" style="width: 66%">
-                    <h2>Чертежи</h2>
-                    <div id="w0" class="grid-view">
-                        <table class="table table-striped table-bordered">
-                            <?php
-                            /**
-                             * @var $manualProduct ManualProduct
-                             */
-                            ?>
-                            <?php foreach ($product->manuals as $manualProduct): ?>
-                                <tr class="manual-product-row">
-                                    <td><?php $breadcrumbs = $manualProduct->manualCategory->createBreadcrumbs();
-                                        unset($breadcrumbs[0]);
-                                        echo Breadcrumbs::widget([
-                                            'links' => $breadcrumbs,
-                                            'homeLink' => false
-                                        ]); ?></td>
-                                    <td>
-                                        <?= Html::a('<div class="site-btn open-catalog">Перейти</div>', [
-                                            'manual/image',
-                                            'id' => $manualProduct->manualCategory->manual_id,
-                                            'categoryId' => $manualProduct->manualCategory->id
-                                        ], ['target' => '_blank']); ?>
-                                    </td>
-                                </tr>
+    <div>
+        <div class="100%">
+            <div style="width: 60%">
+                <div class="product-content">
+                    <div class="product-images" style="vertical-align: center; text-align: center">
+                        <?php if ($product->images): ?>
+                            <?php foreach ($product->images as $productImage): ?>
+                                <div class="product-image">
+                                    <a href="<?php echo $productImage->getImagePath(false); ?>" data-fancybox="1"
+                                       rel="group">
+                                        <img src="<?php echo $productImage->getImagePath(); ?>"
+                                             alt="<?php echo $product->title; ?>"/>
+                                    </a>
+                                </div>
                             <?php endforeach; ?>
-                        </table>
+                        <?php else: ?>
+                            <img src="/img/empty-s.png" alt="<?php echo $product->title; ?>">
+                        <?php endif; ?>
                     </div>
+                    <div class="product-info">
+                        Код на сайте: <b><?php echo $product->getCode(); ?></b>
+                        <br/>
+                        <?php if ($product->shop_code): ?>
+                            Артикул: <b><?php echo $product->shop_code; ?></b>
+                            <br/>
+                        <?php endif; ?>
+                        Цена: <b><span><?php echo $product->price; ?></span> руб</b>
+                        <br/>
+                        <div class="site-btn add-cart-product" data-id="<?php echo $product->id; ?>">
+                            Добавить в корзину
+                        </div>
+                    </div>
+                    <?php if ($product->oversize): ?>
+                        <?php echo Html::icon('scale', [
+                            'class' => 'bulky-product',
+                            'data-tooltip' => 'tooltip',
+                            'data-trigger' => 'hover',
+                            'data-placement' => 'right',
+                            'title' => 'Крупногабаритный товар',
+                        ]); ?>
+                    <?php endif; ?>
                 </div>
-            <?php endif; ?>
+            </div>
         </div>
+        <?php if (sizeof($product->manuals) > 0): ?>
+            <div class="manual-product-view" style="width: 100%">
+                <h2>Чертежи</h2>
+                <div id="w0" class="grid-view">
+                    <table class="table table-striped table-bordered">
+                        <?php
+                        /**
+                         * @var $manualProduct ManualProduct
+                         */
+                        ?>
+                        <?php foreach ($product->manuals as $manualProduct): ?>
+                            <tr class="manual-product-row">
+                                <td><?php $breadcrumbs = $manualProduct->manualCategory->createBreadcrumbs();
+                                    unset($breadcrumbs[0]);
+                                    echo Breadcrumbs::widget([
+                                        'links' => $breadcrumbs,
+                                        'homeLink' => false
+                                    ]); ?></td>
+                                <td>
+                                    <?= Html::a('<div class="site-btn open-catalog">Перейти</div>', [
+                                        'manual/image',
+                                        'id' => $manualProduct->manualCategory->manual_id,
+                                        'categoryId' => $manualProduct->manualCategory->id
+                                    ], ['target' => '_blank']); ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
     <div>
         <?php if (sizeof($product->similarProducts) > 0): ?>
