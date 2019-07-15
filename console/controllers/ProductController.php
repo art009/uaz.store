cc8c1cb3f5a99dc3515a88a336792759392748e5
Mikhail Kuklin <mihan007@ya.ru>
2019-07-15 15:08:05 +0400

    Перестройка аналогичных товаров

--- console/controllers/ProductController.php
+++ console/controllers/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends Controller
 {
     public function actionFillSimilarProducts()
     {
+        Yii::$app->db->createCommand()->truncateTable('catalog_product_similar')->execute();
         $catalogProducts = CatalogProduct::find();
         $catalogProductsSimilar = [];
         foreach ($catalogProducts->each() as $catalogProduct) {
