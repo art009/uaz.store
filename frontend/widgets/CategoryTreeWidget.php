911d54d8354de7a83c65f0c04fde2dd1f3b1f97e
Mikhail Kuklin <mihan007@ya.ru>
2019-07-22 09:35:09 +0300

    В поиске по группам пропало дерево каталогов
    close #36

--- frontend/widgets/CategoryTreeWidget.php
+++ frontend/widgets/CategoryTreeWidget.php
@@ -17,109 +17,104 @@ use yii\caching\TagDependency;
  */
 class CategoryTreeWidget extends Widget
 {
-	/**
-	 * @var string
-	 */
-	public $baseLink = '';
+    /**
+     * @var string
+     */
+    public $baseLink = '';
 
-	/**
-	 * @var
-	 */
-	public $categoryId = null;
+    /**
+     * @var
+     */
+    public $categoryId = null;
 
-	/**
-	 * @var bool
-	 */
-	public $toggleableParent = false;
+    /**
+     * @var bool
+     */
+    public $toggleableParent = false;
 
-	/**
-	 * @var TreeItem[]
-	 */
-	protected $items = [];
+    /**
+     * @var TreeItem[]
+     */
+    protected $items = [];
 
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		if (empty($this->items)) {
-			$categories = $this->getCategories();
-			$this->buildTree($categories);
-		}
-	}
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (empty($this->items)) {
+            $categories = $this->getCategories();
+            $this->buildTree($categories);
+        }
+    }
 
 
-	/**
-	 * @inheritdoc
-	 */
-	public function run()
-	{
-		return $this->render('category-tree', [
-			'items' => $this->items,
-		]);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        return $this->render('category-tree', [
+            'items' => $this->items,
+        ]);
+    }
 
-	/**
-	 * @return array
-	 */
-	protected function getCategories()
-	{
-        TagDependency::invalidate(\Yii::$app->cache, CatalogCategory::CATEGORY_TREE_CACHE_TAG);
+    /**
+     * @return array
+     */
+    protected function getCategories()
+    {
+        //todo: cache this
+        return CatalogCategory::find()
+            ->select(['id', 'parent_id', 'title', 'link'])
+            ->where([
+                'hide' => AppHelper::NO,
+            ])
+            ->orderBy('parent_id ASC, title ASC')
+            ->asArray()
+            ->all();
+    }
 
-		$categories = CatalogCategory::getDb()->cache(function(){
-			return CatalogCategory::find()
-				->select(['id', 'parent_id', 'title', 'link'])
-				->where([
-					'hide' => AppHelper::NO,
-				])
-				->orderBy('parent_id ASC, title ASC')
-				->asArray()
-				->all();
-		}, 0, new TagDependency(['tags' => CatalogCategory::CATEGORY_TREE_CACHE_TAG]));
+    /**
+     * @param $categories
+     */
+    protected function buildTree($categories)
+    {
+        if ($categories) {
+            foreach ($categories as $category) {
+                $item = new TreeItem($category);
+                if ($item->isValid()) {
+                    $item->active = ($item->id == $this->categoryId);
+                    $item->expanded = $item->active;
+                    if ($item->parentId) {
+                        $this->addChildItem($this->items, $item);
+                    } else {
+                        $item->updateLink($this->baseLink);
+                        $this->items[$item->id] = $item;
+                    }
+                }
+            }
+        }
+    }
 
-		return $categories;
-	}
+    /**
+     * @param TreeItem[] $items
+     * @param TreeItem $child
+     *
+     * @return bool
+     */
+    protected function addChildItem($items, $child)
+    {
+        foreach ($items as $id => $item) {
+            if ($item->id == $child->parentId) {
+                $item->addChild($child);
+                $item->toggleable = $this->toggleableParent;
+                return $item->expanded;
+            } elseif ($item->items) {
+                $item->expanded |= $this->addChildItem($item->items, $child);
+            }
+        }
 
-	/**
-	 * @param $categories
-	 */
-	protected function buildTree($categories)
-	{
-		if ($categories) {
-			foreach ($categories as $category) {
-				$item = new TreeItem($category);
-				if ($item->isValid()) {
-					$item->active = ($item->id == $this->categoryId);
-					$item->expanded = $item->active;
-					if ($item->parentId) {
-						$this->addChildItem($this->items, $item);
-					} else {
-						$item->updateLink($this->baseLink);
-						$this->items[$item->id] = $item;
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * @param TreeItem[] $items
-	 * @param TreeItem $child
-	 *
-	 * @return bool
-	 */
-	protected function addChildItem($items, $child)
-	{
-		foreach ($items as $id => $item) {
-			if ($item->id == $child->parentId) {
-				$item->addChild($child);
-				$item->toggleable = $this->toggleableParent;
-				return $item->expanded;
-			} elseif ($item->items) {
-				$item->expanded |= $this->addChildItem($item->items, $child);
-			}
-		}
-
-		return false;
-	}
+        return false;
+    }
 }
