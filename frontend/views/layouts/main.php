45b38ddbb38066349a9dc499f4d7acc523bae0d7
Mikhail Kuklin <mikhail.kuklin@parity.digital>
2019-11-17 17:13:46 +0300

    Set yandex.metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -37,7 +37,7 @@ $this->addSeoMetatags();
 <body>
 <?php $this->beginBody() ?>
 <?php if ($GATrackingID = (Yii::$app->params['GATrackingID'] ?? null)): ?>
-    <!--Google analytics-->
+    <!-- Google analytics -->
     <script>
       (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r
@@ -61,8 +61,12 @@ $this->addSeoMetatags();
         setTimeout(function () {
           ga('send', 'event', 'Новый посетитель', location.pathname)
         }, 15000)</script>
+    <!-- /Google analytics -->
 
-    <!--Google analytics-->
+    <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(56268205, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/56268205" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 <?php endif; ?>
 <div class="wrap">
     <?php NavBar::begin([
